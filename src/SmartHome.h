039527229854adacf0432ff9fb3c5ce6b951466e
chaosad <frasker@hotmail.com>
2017-06-24 17:40:32 +0800

    Added SmartHome files in src and test folders

---
+++ src/SmartHome.h
@@ -0,0 +1,5 @@
+#ifndef _SMARTHOME_H
+#define _SMARTHOME_H
+
+
+#endif // _SMARTHOME_H
---
+++ test/test_SmartHome.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "smarthome.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_smarthome_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement smarthome");
+}
